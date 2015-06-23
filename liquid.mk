f72aa5bbff0f06732b9c5650793456f9a98b9093
Tony Malagisi <mtb3000gt@gmail.com>
2015-06-23 17:04:43 -0400

    Fix the makefie for wifi only
    Change-Id: Ic2f13847b01b9b5b26d1b27eebf3dd025c7e78b3

--- liquid.mk
+++ liquid.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/lt03wifi/full_lt03wifi.mk)
 
 # Inherit some common LiquidSmooth stuff.
-$(call inherit-product, vendor/liquid/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/liquid/config/common_tablet_wifionly.mk)
 
 PRODUCT_NAME := liquid_lt03wifi
 PRODUCT_DEVICE := lt03wifi
